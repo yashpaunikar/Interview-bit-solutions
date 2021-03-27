4f8b52590aabf2e459038a42dbd7cca56de84b6a
Yash Paunikar <ypaunikar@gmail.com>
2021-03-28 04:29:50 +0530

    Create LargestNumber.java

---
+++ LargestNumber.java
@@ -0,0 +1,60 @@
+// solution to largest number
+// o(nlogn) + o(n) + o(n)
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public String largestNumber(final List<Integer> A) {
+        String [] num = new String[A.size()];
+        for(int i = 0; i<A.size(); i++){
+            num[i] = String.valueOf(A.get(i));
+        }
+        
+        Arrays.sort(num, new Comparator<String>(){
+            public int compare(String a, String b){
+                String A = a+b;
+                String B = b+a;
+                if(A.compareTo(B) > 0) return -1;
+                else return 1;
+            }
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i<num.length; i++){
+            sb.append(num[i]);
+        }
+        
+        int flag = 0;
+        for(int i = 0; i<sb.length(); i++){
+            if(sb.charAt(i) != '0'){
+                flag = 1;
+                break;
+            } 
+        }
+        
+        if(flag == 0) return "0";
+        else return sb.toString();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
