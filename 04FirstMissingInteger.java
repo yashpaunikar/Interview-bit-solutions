2fc22aaca1e6a875ff130cca6baf26813567ec5c
Yash Paunikar <ypaunikar@gmail.com>
2021-03-29 04:59:55 +0530

    Update 04FirstMissingInteger.java

--- 04FirstMissingInteger.java
+++ 04FirstMissingInteger.java
@@ -14,15 +14,19 @@ public class Solution {
             int num = A[i];
             int pos = num  - 1;
             if(pos >= 0 && pos < n && A[pos] != num){
+                //After swapping decrement i, as the swapped element again needs to be dealt with
                 int temp = num;
                 A[i] = A[pos];
                 A[pos] = temp;
                 i--;
             }
         }
+        
+        //check if the element is present 
         for(int i = 0; i < n; i++){
             if(A[i] != i+1) return i+1;
         }
+        // when 1 to n are present then return n+1, which will be smallest now. 
         return n+1;
     }
 }
