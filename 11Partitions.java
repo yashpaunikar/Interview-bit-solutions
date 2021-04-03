9e83964732b26d6c94678bde0ed5b9901b40d807
Yash Paunikar <ypaunikar@gmail.com>
2021-04-03 08:31:10 +0530

    Create 11Partitions.java

---
+++ 11Partitions.java
@@ -0,0 +1,37 @@
+/*
+Approach link- https://www.youtube.com/watch?v=yEYi1lNMHE0&ab_channel=AnmolGera
+maintain count of sumofarray/3 and 2*sumofarray/3
+iterate till 2nd last element
+get count of number of times you encounter sum/3
+After that whenever we encounter 2*sum/3 we add 
+count of sum/3 to it.
+Finally, we will get number of partitions possible
+*/
+
+public class Solution {
+    public int solve(int A, int[] B) {
+        int sum = 0;
+        for(int i=0; i<B.length; i++){
+            sum += B[i];
+        }
+        // if not divisible by 3 then
+        // no solution exists
+        if(sum % 3 != 0) return 0;
+        
+        int countSumBy3 = 0;
+        int count2SumBy3 = 0;
+        int sum1 = 0;
+        for(int i = 0; i < B.length-1; i++){
+            sum1 += B[i];
+          //everytime we encounter 2*sum/3 we add 
+          //count of sum/3 to it
+            if(sum1 == 2*sum/3){
+                count2SumBy3 += countSumBy3;
+            }
+            if(sum1 == sum/3){
+                countSumBy3++;
+            }
+        }
+        return count2SumBy3;
+    }
+}
